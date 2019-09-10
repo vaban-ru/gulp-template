32b283c043f1d34be5e5ebe4787683231fec240e
Vladimir Banduristov <hello@webzodimir.ru>
2019-09-10 21:11:44 +0300

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,164 @@
+'use strict';
+
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const sass = require('gulp-sass');
+const del = require('del');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const babel = require('gulp-babel');
+const browserSync = require('browser-sync').create();
+const changed = require('gulp-changed');
+const fileinclude = require('gulp-file-include');
+const prettier = require('gulp-prettier');
+const beautify = require('gulp-jsbeautifier');
+
+// Очистка папки dist
+function clean() {
+	return del(['./dist']);
+}
+
+// Инициализация веб-сервера
+function browserSyncInit(done) {
+	browserSync.init({
+		server: {
+			baseDir: './dist',
+		},
+		// tunnel: true,
+		host: 'localhost',
+		port: 9000,
+		logPrefix: 'Webit_Dev',
+	});
+	done();
+}
+
+// Функция перезагрузки страницы
+function browserSyncReload(done) {
+	browserSync.reload();
+	done();
+}
+
+// Шаблонизация и склейка HTML
+function htmlProcess() {
+	return gulp
+		.src(['src/*.html'])
+		.pipe(
+			fileinclude({
+				prefix: '@@',
+				basepath: '@file',
+			})
+		)
+		.pipe(gulp.dest('./dist/'));
+}
+
+// Работа с картинками (обрабатываются только измененные картинки)
+function imgProcess() {
+	return gulp
+		.src('src/img/**/*.*')
+		.pipe(changed('dist/img/'))
+		.pipe(imagemin([
+			imagemin.gifsicle({interlaced: true}),
+			imagemin.jpegtran({progressive: true}),
+			imagemin.optipng({optimizationLevel: 5}),
+			imagemin.svgo({
+				plugins: [
+					{removeViewBox: true},
+					{cleanupIDs: false}
+				]
+			})
+		]))
+		.pipe(gulp.dest('dist/img/'));
+}
+
+function imagesProcess() {
+	return gulp
+		.src('src/images/**/*.*')
+		.pipe(changed('dist/images/'))
+		.pipe(imagemin([
+			imagemin.gifsicle({interlaced: true}),
+			imagemin.jpegtran({progressive: true}),
+			imagemin.optipng({optimizationLevel: 5}),
+			imagemin.svgo({
+				plugins: [
+					{removeViewBox: true},
+					{cleanupIDs: false}
+				]
+			})
+		]))
+		.pipe(gulp.dest('dist/images/'));
+}
+
+// Копирование шрифтов
+function fontsProcess() {
+	return gulp.src(['src/fonts/**/*']).pipe(gulp.dest('./dist/fonts'));
+}
+
+// Склейка и минификация CSS
+function cssProcess() {
+	var plugins = [autoprefixer(), cssnano()];
+	return gulp
+		.src(['src/css/reset.css', 'src/css/grid.css','src/css/**/*.*'])
+		.pipe(concat('libs.min.css'))
+		.pipe(postcss(plugins))
+		.pipe(gulp.dest('./dist/css'));
+}
+
+// Компиляция SASS
+function scssProcess() {
+	var plugins = [autoprefixer()];
+	return gulp
+		.src(['src/sass/app.scss'])
+		.pipe(sass())
+		.pipe(postcss(plugins))
+		.pipe(prettier({ singleQuote: true }))
+		.pipe(gulp.dest('./dist/css'));
+}
+
+// Склейка и минификация JS библиотек
+function libsJsProcess() {
+	return gulp
+		.src([
+			'node_modules/jquery/dist/jquery.min.js',
+			'src/js/!(app)*.js',
+		])
+		.pipe(concat('libs.min.js'))
+		.pipe(babel())
+		.pipe(uglify())
+		.pipe(gulp.dest('./dist/js'));
+}
+
+// Склейка пользовательского JS
+function jsProcess() {
+	return (
+		gulp
+			.src(['src/js/app.js'])
+			.pipe(beautify())
+			.pipe(gulp.dest('./dist/js'))
+	);
+}
+// Наблюдение за изменениями
+function watchFiles() {
+	gulp.watch('src/**/*.html', gulp.series(htmlProcess, browserSyncReload));
+	gulp.watch('src/css/**/*.*', gulp.series(cssProcess, browserSyncReload));
+	gulp.watch('src/sass/**/*.*', gulp.series(scssProcess, browserSyncReload));
+	gulp.watch('src/js/!(app)*.js', gulp.series(libsJsProcess, browserSyncReload));
+	gulp.watch('src/js/app.js', gulp.series(jsProcess, browserSyncReload));
+	gulp.watch('src/img/**/*.*', gulp.series(imgProcess, browserSyncReload));
+	gulp.watch('src/fonts/*.*', gulp.series(fontsProcess, browserSyncReload));
+}
+
+// Определение основных переменных
+const build = gulp.series(
+	clean,
+	gulp.parallel(htmlProcess,cssProcess,scssProcess,libsJsProcess,jsProcess,fontsProcess,imgProcess,imagesProcess)
+);
+
+const watch = gulp.parallel(build, watchFiles, browserSyncInit);
+
+// Экспорт
+exports.build = build;
+exports.default = watch;
+exports.watch = watch;
