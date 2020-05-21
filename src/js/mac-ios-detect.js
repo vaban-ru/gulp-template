f965fd3b087320f8edab471e0220deb388c8393c
Vladimir Banduristov <hello@webzlodimir.ru>
2020-05-21 11:25:47 +0300

    Удалены лишние файлы, добавлены переменны для отступов и шрифтов, установлены брейкпоинты для адаптива

---
+++ src/js/mac-ios-detect.js
@@ -0,0 +1,7 @@
+var root = document.getElementsByTagName('html')[0];
+if (/(Mac|iPhone|iPod|iPad)/i.test(navigator.platform)) {
+  root.classList.add('is-mac');
+}
+if (/(iPhone|iPod|iPad)/i.test(navigator.platform)) {
+  root.classList.add('is-ios');
+}
